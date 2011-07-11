76003f768c855162484a04ca03b60b1a48996b8d
TJ Singleton <tjsingleton@vantagestreet.com>
2011-07-11 11:11:08 -0400

    semicolon not needed

--- deskshare/applet/src/main/java/org/bigbluebutton/deskshare/client/net/Message.java
+++ deskshare/applet/src/main/java/org/bigbluebutton/deskshare/client/net/Message.java
@@ -21,7 +21,7 @@ package org.bigbluebutton.deskshare.client.net;
 
 public interface Message {
 
-	public enum MessageType {BLOCK, CURSOR};
-	
-	public MessageType getMessageType();
+	public enum MessageType {BLOCK, CURSOR}
+
+    public MessageType getMessageType();
 }
