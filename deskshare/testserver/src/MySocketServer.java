ca4b064327be89f311709181f1337779e83264cc
TJ Singleton <tjsingleton@vantagestreet.com>
2011-07-18 21:09:41 -0400

    fix typo

--- deskshare/testserver/src/MySocketServer.java
+++ deskshare/testserver/src/MySocketServer.java
@@ -5,12 +5,12 @@ import java.net.Socket;
 
 public class MySocketServer {
 
-    public static final int RECIEVE_BUFFER_SIZE = 512000;
+    private static final int RECEIVE_BUFFER_SIZE = 512000;
 
     public static void main(String args[]) throws IOException, InterruptedException {
 	ServerSocket server = new ServerSocket(9124);
 	server.setPerformancePreferences(0, 1, 2);
-	server.setReceiveBufferSize(RECIEVE_BUFFER_SIZE);
+	server.setReceiveBufferSize(RECEIVE_BUFFER_SIZE);
 
 	Socket socket = server.accept();
 
@@ -19,14 +19,14 @@ public class MySocketServer {
 	int bytes = 0;
 
 	while (true) {
-	    int size = input.read(new byte[RECIEVE_BUFFER_SIZE]);
+	    int size = input.read(new byte[RECEIVE_BUFFER_SIZE]);
 
-	    if(size > 0) {
+	    if (size > 0) {
 		bytes += size;
-		float secs = ((System.currentTimeMillis() - start)/1000F);
-		float kb   = bytes/1024F;
-		float rate = kb/secs;
-		System.out.println("recv: " + size/1024F + " kbytes; kbytes/s: " + rate);
+		float secs = ((System.currentTimeMillis() - start) / 1000F);
+		float kb = bytes / 1024F;
+		float rate = kb / secs;
+		System.out.println("recv: " + size / 1024F + " kbytes; kbytes/s: " + rate);
 	    }
 	}
     }
