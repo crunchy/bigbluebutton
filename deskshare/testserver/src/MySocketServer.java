156a4435014ec18994409999f826ea893e157e16
TJ Singleton <tjsingleton@vantagestreet.com>
2011-07-18 20:01:46 -0400

    tests socket speed

---
+++ deskshare/testserver/src/MySocketServer.java
@@ -0,0 +1,34 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MySocketServer {
+
+    public static final int RECIEVE_BUFFER_SIZE = 512000;
+
+    public static void main(String args[]) throws IOException, InterruptedException {
+	ServerSocket server = new ServerSocket(9124);
+	server.setPerformancePreferences(0, 1, 2);
+	server.setReceiveBufferSize(RECIEVE_BUFFER_SIZE);
+
+	Socket socket = server.accept();
+
+	BufferedInputStream input = new BufferedInputStream(socket.getInputStream());
+	long start = System.currentTimeMillis();
+	int bytes = 0;
+
+	while (true) {
+	    int size = input.read(new byte[RECIEVE_BUFFER_SIZE]);
+
+	    if(size > 0) {
+		bytes += size;
+		float secs = ((System.currentTimeMillis() - start)/1000F);
+		float kb   = bytes/1024F;
+		float rate = kb/secs;
+		System.out.println("recv: " + kb + " kbytes; kbytes/s: " + rate);
+	    }
+	}
+    }
+
+}
